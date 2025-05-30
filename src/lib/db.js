d6d6a40692b1910f0c31dbac7ae37465c71edcfb
sanirmem <sanirmem@students.zhaw.ch>
2025-05-30 12:49:01 +0200

    update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,157 +2,97 @@ import { MongoClient, ObjectId } from "mongodb";
 import { DB_URI } from "$env/static/private";
 
 const client = new MongoClient(DB_URI);
-
 await client.connect();
 const db = client.db("F1DB");
 
-//Get all teams
+// -------- Teams --------
 async function getTeams() {
-    let teams = [];
-    try {
-        const collection = db.collection("teams");
-
-        const query = {};
-
-        teams = await collection.find(query).toArray();
-        teams.forEach((team) => {
-            team._id = team._id.toString();
-        });
-    } catch (error) {
-        console.log(error);
-    }
-    return teams;
+  try {
+    const teams = await db.collection("teams").find({}).toArray();
+    return teams.map(t => ({ ...t, _id: t._id.toString() }));
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
 
-//Get Team by ID
 async function getTeam(id) {
-    let team = null;
-    try {
-        const collection = db.collection("teams");
-        const query = { _id: new ObjectId(id) };
-        team = await collection.findOne(query);
-
-        if (!team) {
-            console.log("No team with id " + id);
-        } else {
-            team._id = team._id.toString();
-        }
-    } catch (error) {
-        console.log(error.message);
-    }
-    return team;
+  try {
+    const team = await db.collection("teams").findOne({ _id: new ObjectId(id) });
+    return team ? { ...team, _id: team._id.toString() } : null;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
 }
 
-//Create Team
 async function createTeam(team) {
-    team.poster = "/images/placeholder.png";
-    try {
-        const collection = db.collection("teams");
-        const result = await collection.insertOne(team);
-        team._id = result.insertedId.toString();
-        return team;
-    } catch (error) {
-        console.log(error.message);
-    }
+  team.poster = "/images/placeholder.png";
+  try {
+    const result = await db.collection("teams").insertOne(team);
+    return { ...team, _id: result.insertedId.toString() };
+  } catch (error) {
+    console.log(error.message);
     return null;
+  }
 }
 
-//Delete Driver
-async function deleteDriver(id) {
-    try {
-        const collection = db.collection("drivers");
-        const query = { _id: new ObjectId(id) };
-        await collection.deleteOne(query);
-
-        if (result.deletedCount === 0) {
-            console.log("No driver with id " + id);
-        } else {
-            console.log("Driver with id " + id + " has been successfully deleted");
-            return id;
-    } 
-} catch (error) {
-        console.log(error.message);
-    }
-    return null;
-}
-//Delete Team
 async function deleteTeam(id) {
-    try {
-        const collection = db.collection("teams");
-        const query = { _id: new ObjectId(id) };
-        await collection.deleteOne(query);
-
-        if (result.deletedCount === 0) {
-            console.log("No team with id " + id);
-        } else {
-            console.log("Team with id " + id + " has been successfully deleted");
-            return id;
-        }
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    const result = await db.collection("teams").deleteOne({ _id: new ObjectId(id) });
+    return result.deletedCount > 0 ? id : null;
+  } catch (error) {
+    console.log(error.message);
     return null;
+  }
 }
 
-// Get all drivers for a specific team
+// -------- Drivers --------
 async function getDriversByTeam(teamName) {
-    let drivers = [];
-    try {
-        const collection = db.collection("drivers");
-        const query = { team: teamName };
-        drivers = await collection.find(query).toArray();
-        drivers.forEach(driver => {
-            driver._id = driver._id.toString();
-        });
-    } catch (error) {
-        console.log(error.message);
-    }
-    return drivers;
+  try {
+    const drivers = await db.collection("drivers").find({ team: teamName }).toArray();
+    return drivers.map(d => ({ ...d, _id: d._id.toString() }));
+  } catch (error) {
+    console.log(error.message);
+    return [];
+  }
 }
 
 async function getDriver(id) {
-    try {
-        const collection = db.collection("drivers");
-        const query = { _id: new ObjectId(id) };
-        const driver = await collection.findOne(query);
-        if (driver) {
-            driver._id = driver._id.toString();
-        }
-        return driver;
-    } catch (error) {
-        console.log(error.message);
-        return null;
-    }
+  try {
+    const driver = await db.collection("drivers").findOne({ _id: new ObjectId(id) });
+    return driver ? { ...driver, _id: driver._id.toString() } : null;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
 }
 
 async function createDriver(driver) {
-    driver.poster = "/images/placeholder.png";
-    try {
-        const collection = db.collection("drivers");
-        const result = await collection.insertOne(driver);
-        driver._id = result.insertedId.toString();
-        return driver;
-    } catch (error) {
-        console.log(error.message);
-        return null;
-    }
+  driver.poster = "/images/placeholder.png";
+  try {
+    const result = await db.collection("drivers").insertOne(driver);
+    return { ...driver, _id: result.insertedId.toString() };
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
 }
 
 async function deleteDriver(id) {
-    try {
-        const collection = db.collection("drivers");
-        const result = await collection.deleteOne({ _id: new ObjectId(id) });
-        return result.deletedCount > 0 ? id : null;
-    } catch (error) {
-        console.log(error.message);
-        return null;
-    }
+  try {
+    const result = await db.collection("drivers").deleteOne({ _id: new ObjectId(id) });
+    return result.deletedCount > 0 ? id : null;
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
 }
 
+// -------- Tracks --------
 async function getTracks() {
   try {
-    const collection = db.collection("tracks");
-    const tracks = await collection.find().toArray();
+    const tracks = await db.collection("tracks").find({}).toArray();
     return tracks.map(t => ({ ...t, _id: t._id.toString() }));
   } catch (error) {
     console.log("Error loading tracks:", error.message);
@@ -162,8 +102,7 @@ async function getTracks() {
 
 async function createTrack(track) {
   try {
-    const collection = db.collection("tracks");
-    const result = await collection.insertOne(track);
+    const result = await db.collection("tracks").insertOne(track);
     return { ...track, _id: result.insertedId.toString() };
   } catch (error) {
     console.log("Error creating track:", error.message);
@@ -173,8 +112,7 @@ async function createTrack(track) {
 
 async function deleteTrack(id) {
   try {
-    const collection = db.collection("tracks");
-    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+    const result = await db.collection("tracks").deleteOne({ _id: new ObjectId(id) });
     return result.deletedCount > 0 ? id : null;
   } catch (error) {
     console.log("Error deleting track:", error.message);
@@ -183,16 +121,15 @@ async function deleteTrack(id) {
 }
 
 export default {
-    getTeams,
-    getTeam,
-    createTeam,
-    deleteDriver,
-    deleteTeam,
-    getDriversByTeam,
-    getDriver,
-    createDriver,
-    deleteDriver,
-    getTracks,
-    createTrack,
-    deleteTrack
+  getTeams,
+  getTeam,
+  createTeam,
+  deleteTeam,
+  getDriversByTeam,
+  getDriver,
+  createDriver,
+  deleteDriver,
+  getTracks,
+  createTrack,
+  deleteTrack
 };
